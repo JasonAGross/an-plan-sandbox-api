b41a9530615d4b4c895a173a5d96597add4e9ee2
Jason Gross <jason.gross@e-gineering.com>
2020-12-03 11:02:19 -0500

    add some logging

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ app.get('/products', (req, res) => {
   res.send('hello, from express');
   try {
     pool.connect(process.env.DATABASE_URL, (err, client, done) => {
+      if (err) {
+        done();
+        console.log('something bad happened', err);
+      }
+      console.log('connection happened?');
       client.query(
         'SELECT id, full_description__c, name, catalog_level__c, headline__c FROM salesforce.product2 WHERE catalog_level__c IS NOT NULL AND full_description__c IS NOT NULL AND headline__c IS NOT NULL LIMIT 25', (error, results) => {
+          console.log('db result', results);
           done();
           if (error) {
             console.log(error);
