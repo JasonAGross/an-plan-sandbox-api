88b56ea9e0470fd5989f17943d467565b70435ac
Jason Gross <jason.gross@e-gineering.com>
2020-12-03 10:45:32 -0500

    setup trial endpoint

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+const pool = new pg.Pool();
+
+app.get('/products', (req, res) => {
+  res.send('hello, from express');
+  try {
+    pool.connect(process.env.DATABASE_URL, (err, client, done) => {
+      client.query(
+        'SELECT id, full_description__c, name, catalog_level__c, headline__c FROM salesforce.product2 WHERE catalog_level__c IS NOT NULL AND full_description__c IS NOT NULL AND headline__c IS NOT NULL LIMIT 25', (error, results) => {
+          done();
+          if (error) {
+            console.log(error);
+            res.status(400).send(error);
+          }
+          res.status(200).json(results.rows);
+        }
+      )
+    })
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`node app listening on port ${app.get('port')}`);
+});
